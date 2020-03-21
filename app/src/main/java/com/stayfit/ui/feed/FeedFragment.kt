37c22c8f29dac7de3175c3340c38ee0d5af7f45d
Xavi Escote Llopis <xavi.escote.llopis@gmail.com>
2020-03-21 11:12:23 +0100

    Work on first approach of feed recycler scroller

--- app/src/main/java/com/stayfit/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/stayfit/ui/feed/FeedFragment.kt
@@ -2,11 +2,15 @@ package com.stayfit.ui.feed
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.stayfit.R
+import com.stayfit.ui.profile.ProfileViewModel
+import kotlinx.android.synthetic.main.feed_fragment.*
 
 class FeedFragment : Fragment() {
 
@@ -16,10 +20,15 @@ class FeedFragment : Fragment() {
 
     private lateinit var viewModel: FeedViewModel
 
+    private lateinit var blogAdapter: BlogRecyclerAdapter
+
+    private val TAG = "FeedFragment"
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d(TAG, "onCreateView: FeedFragment created...")
         return inflater.inflate(R.layout.feed_fragment, container, false)
     }
 
--- app/src/main/java/com/stayfit/ui/feed/FeedViewModel.kt
+++ app/src/main/java/com/stayfit/ui/feed/FeedViewModel.kt
@@ -1,6 +1,7 @@
 package com.stayfit.ui.feed
 
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
 
 class FeedViewModel : ViewModel() {
     // TODO: Implement the ViewModel
