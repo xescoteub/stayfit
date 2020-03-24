ca4a064aa14d3f1540c13b084fefbb63f2086dca
Jordi <jbonetv5@gmail.com>
2020-03-24 21:49:15 +0100

    Menu arm y extra con RecyclerView

--- app/src/main/java/com/stayfit/ui/workouts/menu/ArmMenu.kt
+++ app/src/main/java/com/stayfit/ui/workouts/menu/ArmMenu.kt
@@ -4,27 +4,57 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.stayfit.R
+import com.stayfit.ui.workouts.Category
+import com.stayfit.ui.workouts.CategoryAdapter
 import com.stayfit.ui.workouts.listexercises.BicepsExercises
 import com.stayfit.ui.workouts.listexercises.ShoulderExercises
 import com.stayfit.ui.workouts.listexercises.TricepsExercises
+import kotlinx.android.synthetic.main.activity_arm_menu.*
 
 class ArmMenu : AppCompatActivity() {
-
+    lateinit var armList: ArrayList<Category>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_arm_menu)
+        armList = ArrayList()
+        addCategories()
+        showList()
     }
-
-    fun startBicepsExercises(view: View) {
+    private fun addCategories() {
+        armList.add(Category("BICEPS","", R.drawable.biceps) )
+        armList.add(Category("TRICEPS","", R.drawable.triceps) )
+        armList.add(Category("SHOULDER","", R.drawable.shoulder) )
+    }
+    private fun showList() {
+        armRecycler.layoutManager = LinearLayoutManager(this)
+        armRecycler.addItemDecoration(DividerItemDecoration(this, 0))
+        armRecycler.adapter = CategoryAdapter(armList)
+        (armRecycler.adapter as CategoryAdapter).setOnItemClickListener(object :
+            CategoryAdapter.ClickListener {
+            override fun onItemClick(position: Int, v: View?) {
+                startConcreteExercise(armList[position].name)
+            }
+            override fun onItemLongClick(position: Int, v: View?) {
+            }
+        })
+    }
+    fun startConcreteExercise(s: String){
+        if (s.equals("BICEPS")){startBicepsExercises()}
+        else if (s.equals("TRICEPS")){startTricepsExercises()}
+        else{startShoulderExercises()}
+    }
+    fun startBicepsExercises() {
         val intent = Intent(this, BicepsExercises::class.java)
         startActivity(intent)
     }
-    fun startTricepsExercises(view: View) {
+    fun startTricepsExercises() {
         val intent = Intent(this, TricepsExercises::class.java)
         startActivity(intent)
     }
-    fun startShoulderExercises(view: View) {
+    fun startShoulderExercises() {
         val intent = Intent(this, ShoulderExercises::class.java)
         startActivity(intent)
     }
--- app/src/main/java/com/stayfit/ui/workouts/menu/ExtraMenu.kt
+++ app/src/main/java/com/stayfit/ui/workouts/menu/ExtraMenu.kt
@@ -4,31 +4,65 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.stayfit.R
+import com.stayfit.ui.workouts.Category
+import com.stayfit.ui.workouts.CategoryAdapter
 import com.stayfit.ui.workouts.listexercises.CardioExercises
 import com.stayfit.ui.workouts.listexercises.MeditationExercises
 import com.stayfit.ui.workouts.listexercises.StretchesExercises
 import com.stayfit.ui.workouts.listexercises.YogaExercises
+import kotlinx.android.synthetic.main.activity_extra_menu.*
 
 class ExtraMenu : AppCompatActivity() {
-
+    lateinit var extraList: ArrayList<Category>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_extra_menu)
+        extraList = ArrayList()
+        addCategories()
+        showList()
     }
-    fun startCardioExercises(view: View) {
+    private fun addCategories() {
+        extraList.add(Category("CARDIO","", R.drawable.cardio) )
+        extraList.add(Category("STRETCHES","", R.drawable.stretches) )
+        extraList.add(Category("YOGA","", R.drawable.yoga) )
+        extraList.add(Category("MEDITATION","", R.drawable.meditation) )
+    }
+    private fun showList() {
+        extraRecycler.layoutManager = LinearLayoutManager(this)
+        extraRecycler.addItemDecoration(DividerItemDecoration(this, 0))
+        extraRecycler.adapter = CategoryAdapter(extraList)
+        (extraRecycler.adapter as CategoryAdapter).setOnItemClickListener(object :
+            CategoryAdapter.ClickListener {
+            override fun onItemClick(position: Int, v: View?) {
+                startConcreteExercise(extraList[position].name)
+            }
+            override fun onItemLongClick(position: Int, v: View?) {
+            }
+        })
+    }
+
+    fun startConcreteExercise(s: String){
+        if (s.equals("CARDIO")){startCardioExercises()}
+        else if (s.equals("STRETCHES")){startStretchesExercises() }
+        else if (s.equals("YOGA")){startYogaExercises()}
+        else{ startMeditationExercises()}
+    }
+    fun startCardioExercises() {
         val intent = Intent(this, CardioExercises::class.java)
         startActivity(intent)
     }
-    fun startStretchesExercises(view: View) {
+    fun startStretchesExercises() {
         val intent = Intent(this, StretchesExercises::class.java)
         startActivity(intent)
     }
-    fun startYogaExercises(view: View) {
+    fun startYogaExercises() {
         val intent = Intent(this, YogaExercises::class.java)
         startActivity(intent)
     }
-    fun startMeditationExercises(view: View) {
+    fun startMeditationExercises() {
         val intent = Intent(this, MeditationExercises::class.java)
         startActivity(intent)
     }
