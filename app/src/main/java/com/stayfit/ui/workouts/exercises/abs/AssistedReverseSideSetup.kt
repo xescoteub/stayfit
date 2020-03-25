7e2e643952c37345aa1cecc62556792eea773fe3
Jordi <jbonetv5@gmail.com>
2020-03-25 17:31:20 +0100

    exercise interface

--- app/src/main/java/com/stayfit/ui/workouts/exercises/abs/AssistedReverseSideSetup.kt
+++ app/src/main/java/com/stayfit/ui/workouts/exercises/abs/AssistedReverseSideSetup.kt
@@ -3,23 +3,31 @@ package com.stayfit.ui.workouts.exercises.abs
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.MediaController
+import android.widget.ProgressBar
 import android.widget.VideoView
 import com.stayfit.R
 
 class AssistedReverseSideSetup : AppCompatActivity() {
-
+    var progressBar: ProgressBar? = null
+    var myCountDownTimer: MyCountDownTimer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_assisted_reverse_side_setup)
+        progressBar = findViewById(R.id.progressBar_assistedreversesidesetup) as ProgressBar?
+        myCountDownTimer = MyCountDownTimer(10000, 1000)
+        myCountDownTimer!!.start()
 
-        val videoView = findViewById<VideoView>(R.id.videoAssistedReverseSideSetup)
-        val path = "android.resource://" + getPackageName() + "/" + R.raw.assisted_reverse_side_situp
-        val uri = Uri.parse(path)
-        val mediaController = MediaController(this)
-        videoView.setMediaController(mediaController)
-        videoView.setVideoURI(uri)
-        videoView.requestFocus()
-        videoView.start()
+    }
+    inner class MyCountDownTimer(millisInFuture: Long, countDownInterval: Long) :
+        CountDownTimer(millisInFuture, countDownInterval) {
+        override fun onTick(millisUntilFinished: Long) {
+            val progress = (millisUntilFinished / 1000).toInt()
+            progressBar!!.progress = progressBar!!.max - progress
+        }
+        override fun onFinish() {
+            finish()
+        }
     }
 }
