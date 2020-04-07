62f1bfe64ca614423e3be3d95f5d94f67a0460bb
Jordi <jbonetv5@gmail.com>
2020-04-07 08:40:11 +0200

    my exercises se quedan guardados aunque cierres la app

--- app/src/main/java/com/stayfit/ui/workouts/listexercises/MyExercises.kt
+++ app/src/main/java/com/stayfit/ui/workouts/listexercises/MyExercises.kt
@@ -1,7 +1,7 @@
 package com.stayfit.ui.workouts.listexercises
 
-import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
@@ -9,9 +9,12 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.stayfit.R
 import com.stayfit.ui.workouts.exercises.Exercise
 import com.stayfit.ui.workouts.exercises.ExerciseActivity
+import java.lang.reflect.Type
 
 class MyExercises : AppCompatActivity() {
     //create object of listview
@@ -23,12 +26,13 @@ class MyExercises : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_exercises)
+        //loadData()
         listView = findViewById<ListView>(R.id.my_exercises_list)
         controlListView()
     }
     fun controlListView(){
-
         //Add elements to arraylist
+        loadData()
         //Create Adapter
         val arrayAdapter: ArrayAdapter<*> = ArrayAdapter<Any>(this, android.R.layout.simple_list_item_1, arrayList as List<Any>?)
         //assign adapter to listview
@@ -48,6 +52,7 @@ class MyExercises : AppCompatActivity() {
     fun addExercise(exercise: Exercise){
         exerciseList.add(exercise)
         arrayList.add(exercise.getExerciseName())
+        saveData()
     }
 
     fun newExercise(view: View) {
@@ -64,4 +69,69 @@ class MyExercises : AppCompatActivity() {
             }
         }
     }
+    private fun saveData(){
+        var sharedPreferences:SharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
+        var editor: SharedPreferences.Editor = sharedPreferences.edit()
+        var gson: Gson = Gson()
+        var jsonNames: String = gson.toJson(arrayList)
+        var jsonExercise: String = gson.toJson(exerciseList)
+        editor.putString("exercise name list", jsonNames)
+        editor.putString("exercise list", jsonExercise)
+        editor.apply()
+    }
+    private fun loadData(){
+        var sharedPreferences:SharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
+        var gson: Gson = Gson()
+        var jsonNames: String = sharedPreferences.getString("exercise name list", null)
+        var jsonExercise: String = sharedPreferences.getString("exercise list",null)
+        val typeName:Type = object : TypeToken<ArrayList<String?>?>() {}.type
+        val typeExercise:Type = object : TypeToken<ArrayList<Exercise?>?>() {}.type
+        arrayList = gson.fromJson(jsonNames, typeName)
+        exerciseList = gson.fromJson(jsonExercise,typeExercise)
+        if (arrayList == null) {
+            arrayList = ArrayList()
+        }
+        if (exerciseList == null) {
+            exerciseList = ArrayList()
+        }
+    }
+
+    /*
+    private fun saveLists() {
+        try {
+            val file: File = Environment.getExternalStorageDirectory()
+            val filename = File(file, "name_my_exercises")
+            val fileExercises = File(file, "my_exercises")
+            var fosName = FileOutputStream(filename)
+            var fosExercise = FileOutputStream(fileExercises)
+            var outName = ObjectOutputStream(fosName)
+            var outExercise = ObjectOutputStream(fosExercise)
+            outName.writeObject(arrayList)
+            outExercise.writeObject(exerciseList)
+            outName.close()
+            outExercise.close()
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+        }
+    }
+
+    private fun readLists() {
+        try {
+            val file: File = Environment.getExternalStorageDirectory()
+            val filename = File(file, "name_my_exercises")
+            val fileExercises = File(file, "my_exercises")
+            var fisName = FileInputStream(filename)
+            var fisExercise = FileInputStream(fileExercises)
+            var inName = ObjectInputStream(fisName)
+            var inExercise = ObjectInputStream(fisExercise)
+            arrayList = inName.readObject() as ArrayList<String>
+            exerciseList = inExercise.readObject() as ArrayList<Exercise>
+            inName.close()
+            inExercise.close()
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+        } catch (ex: ClassNotFoundException) {
+            ex.printStackTrace()
+        }
+    }*/
 }
