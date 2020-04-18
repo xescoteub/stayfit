c3b15bea8571d8fd2aae4b651b8ace56a3dc4fc1
mriverre8 <rivemark00@gmail.com>
2020-04-18 13:32:41 +0200

    PickerDate Calendar

--- app/src/main/java/com/stayfit/ui/activity/ActivityFragment.kt
+++ app/src/main/java/com/stayfit/ui/activity/ActivityFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.stayfit.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ActivityFragment : Fragment() {
 
@@ -15,12 +19,28 @@ class ActivityFragment : Fragment() {
     }
 
     private lateinit var viewModel: ActivityViewModel
+    var theDate: TextView?= null
+    var mCalendarView:android.widget.CalendarView ?= null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.calendar_fragment, container, false)
+        var view:View = inflater.inflate(R.layout.calendar_fragment, container, false)
+
+        mCalendarView = view.findViewById(R.id.calendarView)
+
+        val date:String = SimpleDateFormat("dd-MM-yyy", Locale.getDefault()).format(Date())
+        theDate=view.findViewById(R.id.datePicker)
+        theDate!!.setText("" + date)
+
+        mCalendarView!!.setOnDateChangeListener(CalendarView.OnDateChangeListener { calendarView, year, month, dayOfMonth ->
+            val date:String = dayOfMonth.toString() + "-" + "0" + (month + 1) + "-" + year
+            theDate=view.findViewById(R.id.datePicker)
+            theDate!!.setText("" + date)
+        })
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -29,3 +49,4 @@ class ActivityFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 }
+
