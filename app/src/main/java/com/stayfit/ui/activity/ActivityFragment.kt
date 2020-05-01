0cf40d2f644f628e1c8e699412137f18630b7357
mriverre8 <rivemark00@gmail.com>
2020-05-01 19:11:24 +0200

    Recycler para display calendar, adapter calendar, listview en layout calendar.

--- app/src/main/java/com/stayfit/ui/activity/ActivityFragment.kt
+++ app/src/main/java/com/stayfit/ui/activity/ActivityFragment.kt
@@ -1,17 +1,27 @@
 package com.stayfit.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CalendarView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.stayfit.R
 import com.stayfit.ui.myroutines.Routine
+import com.stayfit.ui.myroutines.RoutineActivity
+import com.stayfit.ui.myroutines.RoutineAdapter
+import com.stayfit.ui.myroutines.RoutineAdapterCalendar
+import kotlinx.android.synthetic.main.calendar_fragment.*
+import kotlinx.android.synthetic.main.fragment_my_routines.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.ArrayList
 
 class ActivityFragment : Fragment() {
 
@@ -22,7 +32,7 @@ class ActivityFragment : Fragment() {
     private lateinit var viewModel: ActivityViewModel
     var theDate: TextView ?= null
     var mCalendarView:android.widget.CalendarView ?= null
-    var events:MutableMap<Routine,String> = mutableMapOf()
+    lateinit var events:MutableMap<String,ArrayList<Routine>>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,11 +46,22 @@ class ActivityFragment : Fragment() {
         date = date + " EVENTS :"
         theDate=view.findViewById(R.id.datePicker)
         theDate!!.setText("" + date)
+        if(!events.containsKey(date)){
+            Toast.makeText(activity,"No events", Toast.LENGTH_SHORT).show()
+        }else{
+
+            //showList(date)
+        }
 
         mCalendarView!!.setOnDateChangeListener(CalendarView.OnDateChangeListener { calendarView, year, month, dayOfMonth ->
             var date:String = dayOfMonth.toString() + "-" + "0" + (month + 1) + "-" + year + " EVENTS :"
             theDate=view.findViewById(R.id.datePicker)
             theDate!!.setText("" + date)
+            if(!events.containsKey(date)){
+                Toast.makeText(activity,"No events", Toast.LENGTH_SHORT).show()
+            }else{
+               //showList(date)
+            }
         })
 
         return view
@@ -49,12 +70,35 @@ class ActivityFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(ActivityViewModel::class.java)
+        events = mutableMapOf()
         // TODO: Use the ViewModel
     }
 
     fun addEvent(year:String,month:String,dayOfMonth:String,routine:Routine){
         val dateEvent:String = dayOfMonth + "-" + "0" + month + "-" + year + " EVENTS :"
-        events.put(routine,dateEvent)
+        if(!events.containsKey(dateEvent)){
+            events.put(dateEvent,arrayListOf(routine))
+        }else{
+            var list:ArrayList<Routine> = events[dateEvent]!!
+            list.add(routine)
+            events.put(dateEvent,list)
+        }
+
     }
+
+    /*private fun showList(eventDay:String) {
+        recyclerCalendar.layoutManager = LinearLayoutManager(activity)
+        var list:ArrayList<Routine> = events[eventDay]!!
+        recyclerCalendar.adapter = RoutineAdapterCalendar(list)
+        (recyclerCalendar.adapter as RoutineAdapterCalendar).setOnItemClickListener(object :
+            RoutineAdapterCalendar.ClickListener {
+            override fun onItemClick(position: Int, v: View?) {
+                //startConcreteRoutine(list[position])
+            }
+            override fun onItemLongClick(position: Int, v: View?) {
+            }
+        })
+    }*/
+
 }
 
---
+++ app/src/main/java/com/stayfit/ui/activity/RoutineAdapterCalendar.kt
@@ -0,0 +1,101 @@
+package com.stayfit.ui.myroutines
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.provider.MediaStore
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.recyclerview.widget.RecyclerView
+import com.stayfit.R
+import kotlinx.android.synthetic.main.routines_recycler_item.view.*
+import java.io.BufferedInputStream
+import java.io.IOException
+import java.io.InputStream
+import java.net.URL
+import java.net.URLConnection
+
+
+class RoutineAdapterCalendar(var items: ArrayList<Routine>) : RecyclerView.Adapter<RoutineAdapterCalendar.RoutineViewHolder>() {
+    class RoutineViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener, View.OnLongClickListener {
+        var routineName  = itemView.blogName
+        var routineDesc  = itemView.blogDescription
+        var routinePhoto = itemView.blogImage
+        override fun onClick(v: View?) {
+            clickListener?.onItemClick(adapterPosition, v)
+        }
+
+        override fun onLongClick(v: View?): Boolean {
+            clickListener?.onItemLongClick(adapterPosition, v)
+            return false
+        }
+
+        init {
+            itemView.setOnClickListener(this)
+            itemView.setOnLongClickListener(this)
+        }
+    }
+
+    fun setOnItemClickListener(clickListener: ClickListener?) {
+        Companion.clickListener = clickListener
+    }
+
+    interface ClickListener {
+        fun onItemClick(position: Int, v: View?)
+        fun onItemLongClick(position: Int, v: View?)
+    }
+
+    companion object {
+        private var clickListener: ClickListener? = null
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RoutineViewHolder {
+        return RoutineViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.routine_recycler_calendar, parent,false ))
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: RoutineViewHolder, position: Int) {
+        // It could be nullable, make it null safe adding ?
+        holder.routineName?.text = items.get(position).name
+        holder.routineDesc?.text = items.get(position).description
+        //holder.routinePhoto.setImageResource(items.get(position).photo)
+        // holder.routinePhoto.setImageBitmap(items.get(position).photo)
+        val url = items.get(position).photo
+        if (!url.equals("null")) {
+            //holder.routinePhoto.setImageBitmap(items.get(position).photo)
+            //holder.routinePhoto.setImageResource(R.drawable.blog_1)
+            if(getImageBitmap(url)!=null){
+                holder.routinePhoto.setImageBitmap(getImageBitmap(url))
+            } else{
+                holder.routinePhoto.setImageResource(R.drawable.blog_1)
+            }
+        }else{
+            holder.routinePhoto.setImageResource(R.drawable.blog_5)
+        }
+
+    }
+    /**
+     * Get bitmap from URL
+     */
+    private fun getImageBitmap(url: String): Bitmap? {
+        var bm: Bitmap? = null
+        try {
+            val aURL = URL(url)
+            val conn: URLConnection = aURL.openConnection()
+            conn.connect()
+            val `is`: InputStream = conn.getInputStream()
+            val bis = BufferedInputStream(`is`)
+            bm = BitmapFactory.decodeStream(bis)
+            bis.close()
+            `is`.close()
+        } catch (e: IOException) {
+            Log.e("BlogAdapter", "Error getting bitmap", e)
+        }
+        return bm
+    }
+}
