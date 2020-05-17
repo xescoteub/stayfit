aea05bd6a584a91c872ceb3eb5849a79f9c66842
mriverre8 <rivemark00@gmail.com>
2020-05-17 21:22:47 +0200

    DR Calendar event

--- app/src/main/java/com/stayfit/ui/myroutines/MyRoutinesFragment.kt
+++ app/src/main/java/com/stayfit/ui/myroutines/MyRoutinesFragment.kt
@@ -63,6 +63,7 @@ class   MyRoutinesFragment: Fragment(){
     }
 
     private fun calendarEvent() {
+        Toast.makeText(activity,"Press the routine that you want to set for a day",Toast.LENGTH_SHORT).show()
         myroutinesRecycler.adapter = RoutineAdapter(routinesList)
         (myroutinesRecycler.adapter as RoutineAdapter).setOnItemClickListener(object :
             RoutineAdapter.ClickListener {
