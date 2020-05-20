61e583345abf66a01ba8b68ae75c3283882ff3b7
mriverre8 <rivemark00@gmail.com>
2020-05-20 19:58:21 +0200

    Information for exersices

--- app/src/main/java/com/stayfit/ui/workouts/listexercises/BicepsExercises.kt
+++ app/src/main/java/com/stayfit/ui/workouts/listexercises/BicepsExercises.kt
@@ -28,15 +28,37 @@ class BicepsExercises : AppCompatActivity() {
     }
     fun controlListView(){
         //Add elements to arraylist
-        var barbellCurl: Exercise = Exercise("Barbell Curl","https://www.youtube.com/watch?v=kwG2ipFRgfo", "0", "null", "" )
+        var barbellCurl: Exercise = Exercise("Barbell Curl","https://www.youtube.com/watch?v=kwG2ipFRgfo", "0", "null", "Dumbbells allow the wrists to move freely, so most people adopt for a slight rotation of the wrist and forearm as they curl, which thickens the muscle group." )
         addExercise(barbellCurl)
 
-        arrayList.add("Chin-Up")
-        arrayList.add("Hammer Curl")
-        arrayList.add("Incline Dumbbell Curl")
-        arrayList.add("Supinated / Reverse Grip Bent Over Row")
-        arrayList.add("Cable Curl")
-        arrayList.add("Concentration Curl")
+        addExercise( Exercise("Chin-Up","https://www.youtube.com/watch?v=T78xCiw_R6g", "0", "null",""))
+        //arrayList.add("Chin-Up")
+        addExercise( Exercise("Hammer Curl","https://www.youtube.com/watch?v=TwD-YGVP4Bk", "0", "null","The hammer will typically be our strongest curl during a biceps workout. This is because all of our elbow flexors are actively involved, and the forearm and wrist are in a power position. Doing this movement like a concentration curl or preacher curl (on a preacher bench) will minimize cheating and maximize muscle recruitment during the workout.\n" +
+                "\n"))
+        //arrayList.add("Hammer Curl")
+        addExercise( Exercise("Rope Hammer Curl","https://www.youtube.com/watch?v=_tg_GRzZiO4", "0", "null",""))
+        //arrayList.add("Cable Rope Hammer Curl")
+        addExercise( Exercise("Incline Dumbbell Curl","https://www.youtube.com/watch?v=b4jOP-spQW8", "0", "null",""))
+        //arrayList.add("Incline Dumbbell Curl")
+        addExercise( Exercise("Supinated / Reverse Grip Bent Over Row","https://www.youtube.com/watch?v=3gdGSSgDby8", "0", "null",""))
+        //arrayList.add("Supinated / Reverse Grip Bent Over Row")
+        addExercise( Exercise("Cable Curl","https://www.youtube.com/watch?v=85kXYq7Ssh4", "0", "null",""))
+        //arrayList.add("Cable Curl")
+        addExercise( Exercise("Concentration Curl","https://www.youtube.com/watch?v=0AUGkch3tzc", "0", "null","Concentration curls place the arm in front of the body with a bent elbow and a rotation in the shoulder. While this decreases recruitment of the long head, it potentially increases biceps thickness and peak by better recruitment of surrounding muscles during your workout.\n" +
+                "\n" +
+                "With your free hand on your off leg to support your body weight, when you hit failure you can switch over to a hammer grip and burn out a few extra reps."))
+        //arrayList.add("Concentration Curl")
+        addExercise( Exercise("EZ-Bar Curl","https://www.youtube.com/watch?v=kwG2ipFRgfo", "0", "null"," It engages both the short and long heads of the biceps muscle and for some people it's a lot more comfortable on the joints and forearms than a straight barbell"))
+        //arrayList.add("EZ-Bar Curl")
+        addExercise( Exercise("Overhead Cable Curl","https://www.youtube.com/watch?v=5_n3gVeGEqc", "0", "null","This is a great way to practice your front double biceps pose as you train. You can do both cables at once, or alternate between arms"))
+        //arrayList.add("Overhead Cable Curl")
+        addExercise( Exercise("Decline Dumbbell Curl","https://www.youtube.com/watch?v=OK6vpxXZ2pk&t=10s", "0", "null",""))
+        //arrayList.add("Decline Bar Curl")
+        addExercise( Exercise("Zottman Curl","https://www.youtube.com/watch?v=ZrpRBgswtHs", "0", "null","In this movement, you hold a dumbbell in each hand and have a palms-up (supinated) grip on the way up and a palms-down (pronated) grip as you lower the weight, so all of your elbow flexors get hit!\n" +
+                "\n" +
+                "Some of your elbow flexors act as supinators as well, so rotating the wrist and forearm during the curl instead of at the bottom will load up that function."))
+        //arrayList.add("Zottman Curl")
+
         //Create Adapter
         val arrayAdapter: ArrayAdapter<*> = ArrayAdapter<Any>(this, android.R.layout.simple_list_item_1, arrayList as List<Any>?)
         //assign adapter to listview
--- app/src/main/java/com/stayfit/ui/workouts/listexercises/TricepsExercises.kt
+++ app/src/main/java/com/stayfit/ui/workouts/listexercises/TricepsExercises.kt
@@ -27,18 +27,92 @@ class TricepsExercises : AppCompatActivity() {
     }
     fun controlListView(){
         //Add elements to arraylist
-        var closegripBenchPress: Exercise = Exercise("Close-grip Bench Press", "https://www.youtube.com/watch?v=wxVRe9pmJdk", "0", "null", "")
+        var closegripBenchPress: Exercise = Exercise("Close-grip Bench Press", "https://www.youtube.com/watch?v=wxVRe9pmJdk", "0", "null", "The bench press is a great tricep exercise to work your chest and core. Placing your hands closer together makes it so your triceps have to work harder, which can lead to new growth and more strength.\n" +
+                "\n" +
+                "How to do it:\n" +
+                "\n" +
+                "- Grasp a barbell with an overhand grip that’s shoulder-width apart, and hold it above your sternum with arms completely straight.\n" +
+                "\n" +
+                "- Lower the bar straight down, pause, and then press the bar back up to the starting position.")
         addExercise(closegripBenchPress)
 
-        arrayList.add("Rope Tricep Pushdown")
-        arrayList.add("Tricep Dips")
-        arrayList.add("Overhead Triceps Extension")
-        arrayList.add("Skullcrushers")
-        arrayList.add("The Diamond Press-up")
-        arrayList.add("Bench Dip")
-        arrayList.add("Dumbbell Floor Press")
-        arrayList.add("The Classic Press-up")
-        arrayList.add("One Arm Kettlebell Floor Press")
+        addExercise( Exercise("Rope Tricep Pushdown","https://www.youtube.com/watch?v=8CbJK7mmisE", "0", "null","This move zones in on your triceps – but only if you do it right. If you use too much weight, you’ll involve your back and shoulder muscles, defeating the purpose. If you can’t keep your shoulders down, lighten the load.\n" +
+                "\n" +
+                "How to do it:\n" +
+                "\n" +
+                "- Attach a rope handle to the high pulley of a cable station. Bend your arms and grab the bar with an overhand grip, your hands shoulder-width apart. Tuck your upper arms next to your sides.\n" +
+                "\n" +
+                "- Without moving your upper arms, push the bar down until your elbows are locked. Slowly return to the starting position."))
+        //arrayList.add("Rope Tricep Pushdown")
+        addExercise( Exercise("Tricep Dips","https://www.youtube.com/watch?v=6kALZikXxLc", "0", "null","\n" +
+                "3. Tricep Dips (Advanced)\n" +
+                "Because you’re lifting your entire bodyweight, your triceps have to work against a much heavier load than they would in a triceps-isolating exercise.\n" +
+                "\n" +
+                "RELATED STORY\n" +
+                "\n" +
+                "Grow Huge Arms with This 6-move Superset Workout\n" +
+                "How to do it:\n" +
+                "\n" +
+                "- Hoist yourself up on parallel bars with your torso perpendicular to the floor; you'll maintain this posture throughout the exercise. (Leaning forward will shift emphasis to your chest and shoulders.)\n" +
+                "\n" +
+                "- Bend your knees and cross your ankles. Slowly lower your body until your shoulder joints are below your elbows. (Most guys stop short of this position.)\n" +
+                "\n" +
+                "- Push back up until your elbows are nearly straight but not locked. If you have shoulder issues, skip this move."))
+        //arrayList.add("Tricep Dips")
+        addExercise( Exercise("Overhead Triceps Extension","https://www.youtube.com/watch?v=_gsUck-7M74", "0", "null","When you work your triceps, you might forget there are three parts to the muscle: the lateral head, the medial head, and the long head. The last part might not always get the attention it deserves – unless you're regularly doing exercises like this one, with your arms over your head to isolate the long head.\n" +
+                "\n" +
+                "How to do it:\n" +
+                "\n" +
+                "- Sit on a bench and grab one dumbbell. Form a diamond shape with both hands to grip the top end of the weight. Raise the dumbbell over your head, keeping your elbows up and your core tight.\n" +
+                "\n" +
+                "- Lower the dumbbell down the top of your back by bending at the elbow, maintaining your strong chest and keeping your shoulders still.\n" +
+                "\n" +
+                "- Raise the weight by fully extending your arms, pausing for a count to squeeze at the top of the movement."))
+        //arrayList.add("Overhead Triceps Extension")
+        addExercise( Exercise("Skullcrushers","https://www.youtube.com/watch?v=ir5PsbniVSc", "0", "null","Whilst there are many variations of this move, they all have one thing in common: elbow extension. As the upper arms are locked in position, the long and lateral tricep heads are called into play. Increasing the angle of an incline bench will work your triceps long head, while doing the movement on a decline bench places more emphasis on the lateral triceps head.\n" +
+                "\n" +
+                "How to do it:\n" +
+                "\n" +
+                "- Grip the EZ bar on the inner grips using an overhand grip and extend your arms straight up.\n" +
+                "\n" +
+                "- Keeping your elbows fixed and tucked in, slowly lower the bar until it is about an inch from your forehead. Always keep your upper arms perpendicular to the floor.\n" +
+                "\n" +
+                "- Slowly extend your arms back to the starting position without locking your elbows."))
+        //arrayList.add("Skullcrushers")
+        addExercise( Exercise("The Diamond Press-up","https://www.youtube.com/watch?v=pD3mD6WgykM", "0", "null","The standard press-up is great for your chest and arms, but moving your hands closer together puts the emphasis squarely on your triceps. You're still going to get some work for your pecs with this variation, but your tris should really feel the burn by the time you're through.\n" +
+                "\n" +
+                "How to do it:\n" +
+                "\n" +
+                "- Lower yourself down into a standard plank or press-up position. Bring your hands close to each other at chest level, with your thumbs touching one another and your forefingers touching. Your spine should be straight, and your core and glutes should be squeezed tight.\n" +
+                "\n" +
+                "- Lower yourself down to the floor. Pause, maintaining the squeeze in your core and glutes, then push back up to the original position by straightening your arms."))
+        //arrayList.add("The Diamond Press-up")
+        addExercise( Exercise("Dumbbell Floor Press","https://www.youtube.com/watch?v=lNdi7VEf2Ew", "0", "null","This variation of a classic bench press favours the lockout portion of the lift, which recruits your triceps to an extreme degree. And since the load is distributed differently with a dumbbell than a barbell, your stabilising muscles have to work harder to keep the weight positioned correctly.\n" +
+                "\n" +
+                "How to do it:\n" +
+                "\n" +
+                "- Grab a dumbbell with each hand and lie with your back on the ground.\n" +
+                "\n" +
+                "- Hold the dumbbells overhead and bend your arm to lower the kettlebells.\n" +
+                "\n" +
+                "- Touch your elbows to the ground, pause, then press them back up."))
+        //arrayList.add("Dumbbell Floor Press")
+        addExercise( Exercise("The Classic Press-up","https://www.youtube.com/watch?v=IODxDxX7oi4", "0", "null","The old ones are the best ones. The traditional press-up works your chest, core and your triceps. The beauty with this move is that it can be performed anywhere. You can make it harder by wearing a weighted vest.\n" +
+                "\n" +
+                "How to do it:\n" +
+                "\n" +
+                "- Set up with your weight supported on your toes and hands beneath your shoulders, body straight. Take care to keep you core locked so a straight line forms between your head, glutes and heels.\n" +
+                "\n" +
+                "- Lower your body until your chest is an inch from the ground then explosively drive up by fully extending your arms."))
+        //arrayList.add("The Classic Press-up")
+        addExercise( Exercise("One Arm Kettlebell Floor Press","https://www.youtube.com/watch?v=B340QckIfJM", "0", "null","Using one arm at a time isolates the chest and triceps, ensuring the muscles are worked hard.\n" +
+                "\n" +
+                "How to do it:\n" +
+                "\n" +
+                "- Lie on the floor and hold a kettlebell in one hand, with your upper arm being supported by the floor.\n" +
+                "\n" +
+                "- Extend your arm and press the kettlebell straight up toward the ceiling. That's one rep. Lower the kettlebell and repeat."))
+        //arrayList.add("One Arm Kettlebell Floor Press")
         //Create Adapter
         val arrayAdapter: ArrayAdapter<*> = ArrayAdapter<Any>(this, android.R.layout.simple_list_item_1, arrayList as List<Any>?)
         //assign adapter to listview
